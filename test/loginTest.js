e0b51934d62860bdd2bcec97fe436b2a60225733
ccckmit <ccckmit@gmail.com>
2017-09-22 11:57:26 +0800

    modify loginTest.js to chai TDD style

--- test/loginTest.js
+++ test/loginTest.js
@@ -1,16 +1,17 @@
 /* eslint-disable no-undef */
-var assert = require('assert')
+// var assert = require('assert')
+var assert = require('chai').assert
 describe('otrak', function () {
   var M = require('./manager')
   describe('login', function () {
-    it('login(ccc, 1111)===undefined', function () {
-      assert.equal('undefined', typeof (M.login('ccc', '1111')))
+    it('login(ccc, 1111).isUndefined', function () {
+      assert.isUndefined(M.login('ccc', '1111'))
     })
     it('login(ccc, 1234567).id===ccc', function () {
       assert.equal('ccc', M.login('ccc', '1234567').id)
     })
-    it('login(leo, 1111)===undefined', function () {
-      assert.equal('undefined', typeof (M.login('leo', '1111')))
+    it('login(leo, 1111).isUndefined', function () {
+      assert.isUndefined(M.login('leo', '1111'))
     })
   })
 })
